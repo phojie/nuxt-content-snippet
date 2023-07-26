1f6afb6c7710de5abf622b0241d254f2213c3a49
phojie rengel <phojrengel@gmail.com>
2023-07-26 20:46:29 +0800

    chore: update config key

--- src/module.ts
+++ src/module.ts
@@ -12,7 +12,7 @@ export interface ModuleOptions {
 export default defineNuxtModule<ModuleOptions>({
   meta: {
     name: 'nuxt-content-snippet',
-    configKey: 'nuxtContentSnippet',
+    configKey: 'contentSnippet',
   },
   defaults: {
     prefix: '@@@',
